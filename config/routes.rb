d4e4b6a4bdaabd460522196bbba7aba102bfc122
Zaaraah <iraharcega@yahoo.com.ph>
2015-03-05 21:20:24 +0800

    edit1 by zarah

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
 
   resources :pages
   resources :reservations
-
-  get 'pages/index'
-#get 'pages/admin'
-
-
-
+  
   root "pages#index"
+  
+  get 'pages/index'
+  get '/admin' => 'pages#admin'
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
