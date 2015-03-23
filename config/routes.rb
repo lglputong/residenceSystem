e35ac731a4ae8e9361816060cb4649f66b3b237d
Zaaraah <iraharcega@yahoo.com.ph>
2015-03-23 15:10:03 +0800

    z

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   get '/contact' => 'pages#contact'
   get '/studentlogin' => 'pages#student_login'
-  get '/test' => 'reservations#all'
+  get '/test' => 'reservations#test'
 
 
   
